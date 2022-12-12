eee199d24a2aa57749426181cc5808ec59dd420b
Gülşah Kadıoğlu <78478922+GulsahKadioglu@users.noreply.github.com>
2022-12-12 20:00:02 +0300

    Update 1523.CountOddNumbersInAnIntervalRange.py

--- Easy/1523.CountOddNumbersInAnIntervalRange.py
+++ Easy/1523.CountOddNumbersInAnIntervalRange.py
@@ -1,10 +1,12 @@
 """
     Given two non-negative integers low and high. Return the count of odd 
     numbers between low and high (inclusive).
+    
     Example:
     Input: low = 3, high = 7
     Output: 3
     Explanation: The odd numbers between 3 and 7 are [3,5,7].
+    
     Constraints:
         - 0 <= low <= high <= 10^9
 """
